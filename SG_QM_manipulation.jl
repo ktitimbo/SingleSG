da00790ec471891803db500451a08bcec897ffc6
Kelvin Titimbo <k.titimbo@gmail.com>
2025-12-19 22:56:40 -0800

    Create SG_QM_manipulation.jl

---
+++ SG_QM_manipulation.jl
@@ -0,0 +1,51 @@
+# Simulation of atom trajectories in the Stern–Gerlach experiment
+# Manipulation of quantum simulations
+# Kelvin Titimbo
+# California Institute of Technology
+# December 2025
+
+using Plots; gr()
+Plots.default(
+    show=true, dpi=800, fontfamily="Computer Modern", 
+    grid=true, minorgrid=true, framestyle=:box, widen=true,
+)
+using Plots.PlotMeasures
+FIG_EXT = "png"   # could be "pdf", "svg", etc.
+SAVE_FIG = true
+# Aesthetics and output formatting
+using Colors, ColorSchemes
+using LaTeXStrings, Printf, PrettyTables
+# Time-stamping/logging
+using Dates
+const T_START = Dates.now() ; # Timestamp start for execution timing
+# Numerical tools
+using LinearAlgebra, DataStructures
+# using Interpolations #, Roots, Loess, Optim
+# using BSplineKit
+using Dierckx, Optim
+using DSP
+using LambertW, PolyLog
+using StatsBase
+using Random, Statistics, NaNStatistics, Distributions, StaticArrays
+using Alert
+# Data manipulation
+using OrderedCollections
+using DelimitedFiles, CSV, DataFrames, JLD2
+# include("./Modules/MyPolylogarithms.jl");
+# Multithreading setup
+using Base.Threads
+LinearAlgebra.BLAS.set_num_threads(4)
+@info "BLAS threads" count = BLAS.get_num_threads()
+@info "Julia threads" count = Threads.nthreads()
+# Set the working directory to the current location
+cd(@__DIR__) ;
+const RUN_STAMP = Dates.format(T_START, "yyyymmddTHHMMSSsss");
+const OUTDIR    = joinpath(@__DIR__, "data_studies", RUN_STAMP);
+isdir(OUTDIR) || mkpath(OUTDIR);
+@info "Created output directory" OUTDIR
+# General setup
+hostname = gethostname();
+@info "Running on host" hostname=hostname
+include("./Modules/TheoreticalSimulation.jl");
+include("./Modules/DataReading.jl");
+include("./Modules/MyExperimentalAnalysis.jl");
