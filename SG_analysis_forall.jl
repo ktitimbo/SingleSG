5d0e585fa43bc3b7958c2036cda3ed99488f9d5b
Kelvin Titimbo <k.titimbo@gmail.com>
2025-12-19 22:57:01 -0800

    Update SG_analysis_forall.jl

--- SG_analysis_forall.jl
+++ SG_analysis_forall.jl
@@ -45,7 +45,7 @@ MyExperimentalAnalysis.SAVE_FIG = SAVE_FIG;
 MyExperimentalAnalysis.FIG_EXT  = FIG_EXT;
 
 # Data Directory
-data_directory      = "20250814" ;
+data_directory      = "20250820" ;
 outfile_raw         = joinpath(data_directory, "data.jld2")
 outfile_processed   = joinpath(data_directory, "data_processed.jld2")
 
