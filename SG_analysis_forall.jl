9bff85d05e124b742bbe601e699df377032749aa
Kelvin Titimbo <k.titimbo@gmail.com>
2026-02-20 21:12:15 -0800

    Update SG_analysis_forall.jl

--- SG_analysis_forall.jl
+++ SG_analysis_forall.jl
@@ -46,13 +46,13 @@ MyExperimentalAnalysis.SAVE_FIG = SAVE_FIG;
 MyExperimentalAnalysis.FIG_EXT  = FIG_EXT;
 
 # Data Directory
-data_directory      = "20260220" ;
+data_directory      = "20250820" ;
 outfile_raw         = joinpath(data_directory, "data.jld2")
 outfile_processed   = joinpath(data_directory, "data_processed.jld2")
 data_summary_path = joinpath(@__DIR__, "analysis_data","summary",data_directory)
 isdir(data_summary_path) || mkpath(data_summary_path);
 # Furnace 
-Temperature = 273.15 + 200
+Temperature = 273.15 + 205
 
 # STERN–GERLACH EXPERIMENT SETUP
 # Camera and pixel geometry : intrinsic properties
